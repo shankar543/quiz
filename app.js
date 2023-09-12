b653b6c9c54663b03f874646dc2fc1b3f9021ef3
shankar534 <janapareddivijayashankar@gmail.com>
2023-09-12 11:42:13 +0530

    code formatting using beautifier and showquestion and shoq answer button

--- app.js
+++ app.js
@@ -47,13 +47,56 @@ function displayCurrentQuestion() {
     container.querySelectorAll('ul li').forEach(li => {
         li.style.backgroundColor = 'transparent';
 })
-         question.innerText = "";
-question.innerText = qtn.question;
+    question.innerText = "";
+    let spanelm = document.createElement('span');
+  
+
+const unformattedCode = qtn.question;
+
+
+const formattedCode = js_beautify(unformattedCode, {
+  indent_size: 2, // Set the desired indentation size
+  space_in_empty_paren: true, // Add spaces in empty parentheses
+});
+
+// Now, you can render the formatted code to the user
+console.log(formattedCode);
+question.innerText = formattedCode;
+    spanelm.innerText = "q";
+    spanelm.classList.add('toggle_q_a')
+    spanelm.style.visibility = 'hidden';
+    spanelm.addEventListener('click', (e) => {
+        question.innerHTML = "";
+        if (e.target.innerText.toLowerCase() == 'q') {
+            e.target.innerText = 'a';
+            question.innerText = formattedCode;
+        }
+        else {
+            if (question.classList.contains('showinganswer')) {
+            showAnswerForCurrentQuestion()
+            e.target.innerText = 'q'                
+            } else {
+                alert('please submit answer the question before seeing the answer');
+                question.innerText = formattedCode;
+                e.target.innerText = 'a';
+            }
+
+        }
+    });
+    if (!container.querySelector('.toggle_q_a')) {
+    container.prepend(spanelm);    
+    }
+spanelm.style.visibility = 'hidden';
 opt_a.innerText = qtn.options.a;
 opt_b.innerText = qtn.options.b;
 opt_c.innerText = qtn.options.c;
 opt_d.innerText = qtn.options.d;
 }
+
+
+
+
+
 function loadQuiz(){
     
     if (!quizzdata?.length) {
@@ -88,15 +131,20 @@ if(isSelected){
 });
     return { isSelected ,useranswer};
 }
-function validate(){
+function validate() {
+    let toggle = document.querySelector('.toggle_q_a');
+    toggle.innerHTML = 'q';
+
 let {isSelected,useranswer} = isAnswered()
     if (isSelected) {
     if(currentQuestionIndex==quizzdata.length){
      container.innerHTML=`<h3>quizz completed</h3><p>you have scored ${score}/${quizzdata.length}.</p><button onclick='document.location.reload()'>start quizz</button>`
     document.querySelector(".controls #nextqtnbtn").remove()
     return;
-    }
+        }
+        showAnswersBydefault = true;
         if (showAnswersBydefault) {
+            toggle.style.visibility = 'visible';
             showAnswer(useranswer);
             return;
         }
@@ -123,17 +171,7 @@ if(opt.value == quizzdata[currentQuestionIndex].correctAnswer){
 }
 });
     question.innerHTML=""
-    let showAnswerText =  quizzdata[currentQuestionIndex]?.explanation;
-    showAnswerText?.split('\n').forEach(line => {
-        let tab = document.createElement('div');
-        tab.classList.add('explination_steps');
-        tab.innerText = line;
-        question.appendChild(tab);
-    });
-    if (!showAnswerText) {
-        question.innerText= "Explination is unavailble to this question \n please proceed to next question"
-    }
-    
+showAnswerForCurrentQuestion()
     question.classList.add('showinganswer');
     submit.classList.add('showanswerenabled');
     let nextqtn = document.createElement("button");
@@ -146,5 +184,18 @@ if(opt.value == quizzdata[currentQuestionIndex].correctAnswer){
     if (!document.querySelector(".controls #nextqtnbtn")) {
          document.querySelector('.controls').appendChild(nextqtn);
     }
+}
+function showAnswerForCurrentQuestion() {
+        let showAnswerText =  quizzdata[currentQuestionIndex]?.explanation;
+    showAnswerText?.split('\n').forEach(line => {
+        let tab = document.createElement('div');
+        tab.classList.add('explination_steps');
+        tab.innerText = line;
+        question.appendChild(tab);
+    });
+    if (!showAnswerText) {
+        question.innerText= "Explination is unavailble to this question \n please proceed to next question"
+    }
+    
 }
 loadQuiz();
